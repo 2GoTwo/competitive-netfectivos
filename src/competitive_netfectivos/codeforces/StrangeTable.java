13ef8301ae156e6a94c4ffce3f42067e60d49ef8
Wassif Meskine <meskine.wassif@gmail.com>
2021-03-26 17:13:57 +0100

    first try

---
+++ src/competitive_netfectivos/codeforces/StrangeTable.java
@@ -0,0 +1,17 @@
+package competitive_netfectivos.codeforces;
+
+import java.util.Scanner;
+
+public class StrangeTable {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			long x = sc.nextLong();
+			
+		}
+		sc.close();
+	}
+}
